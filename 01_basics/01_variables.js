38f30befeb6535fd2ea6f16d42c2503ac1b88e56
Anant Kr Sharma <145072088+AnantKrSharma@users.noreply.github.com>
2023-11-10 14:46:06 +0000

    Variables and Datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "anant@gmail.com"
 var accountPassword = "12345"
 accountCity = "Delhi"
+let accountState;
 
 /* Prefer not to use var
 because of issue in block scope and functional scope */
@@ -12,4 +13,4 @@ accountPassword="12121"
 accountCity="Bengal"
 
 console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
