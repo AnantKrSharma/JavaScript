38e9ae1660bc8ae174e542e799c0367eca3120a6
Anant Kr Sharma <145072088+AnantKrSharma@users.noreply.github.com>
2023-11-11 19:38:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+" use strict " //treat all JS code as newer version.
+
+// alert(3 + 3)  //we are using nodejs, not browser. 
+
+let name = "anant"     //string
+let age = 18           //number
+let isLoggedIn = true  //boolean
+
+console.log(typeof "Hello");  //It tells about the type of the variable.
+console.log(typeof age);
+console.log(typeof null); //It is an Object.
+console.log(typeof undefined) //It is Undefined.
