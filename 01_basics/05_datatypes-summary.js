3b21e9f234de46d57fd90ddb5057c085f07f78e7
Anant Kr Sharma <145072088+AnantKrSharma@users.noreply.github.com>
2023-11-17 13:07:34 +0000

    Dataypes Summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,35 @@
+//--------- Primitive Datatypes ---------
+// 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 200
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId); //Symbols are unique.
+
+const bigNumber = 3455436266565n //BigInt.
+console.log(typeof bigNumber);
+
+
+
+//--------- Non-Primitive or Reference Datatypes ---------
+// 3 Types:  Array, Objects, Functions
+
+const heroes = ["shaktiman", "naagraj", "doga"]; //Array
+
+let myObj = {    //Object
+    name: "anant",
+    age: "20"
+}
+
+const myFunction = function(){  //Function
+    console.log("Hello world");
+}
+
+console.log(typeof heroes);
+console.log(typeof myObj);
+console.log(typeof myFunction);
